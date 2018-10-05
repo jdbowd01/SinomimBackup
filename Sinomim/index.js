7c97baaa8914a604adbaead4746754233dca839e
Jordon <jordon.bowden@gmail.com>
2018-10-05 11:23:14 -0600

    Mock ups, part of API working, proposal, and critical path

---
+++ Sinomim/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var steamCollect = require('./public/script.js')
+
+var app = express();
+app.use(express.static(path.join(__dirname+'/public')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.send({
+    results: steamCollect.methods.findUser(76561198000000000)
+  });
+});
+
+app.listen(3000);
